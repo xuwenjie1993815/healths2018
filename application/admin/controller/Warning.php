9b88b06ab736154b5533f69afc20fefb30d8fc7d
xuwenjie1993815 <380935912@qq.com>
2018-06-22 17:41:14 +0800

    预警管理
    0.2

--- application/admin/controller/Warning.php
+++ application/admin/controller/Warning.php
@@ -11,4 +11,14 @@ class Warning extends Controller{
 		
 		return $this->fetch();
 	}
+
+	//预警处理
+	public function sign_warning(){
+		return array('code' => 1);
+	}
+
+	//今日预警列表
+	public function value_log(){
+		return $this->fetch();
+	}
 }
